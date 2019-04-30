593b744c1034a46d5f3c44224a26828fe69d7f2e
rahmor <rahmor@yahoo.com>
2019-04-30 12:50:30 -0400

    add filter

--- app.js
+++ app.js
@@ -9,9 +9,37 @@ app.use(morgan('common'));
 app.get('/', (req, res) => res.send('hello, world'));
 
 app.get('/apps', (req, res) => {
-  const genres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
+  const genresList = [
+    'Action',
+    'Puzzle',
+    'Strategy',
+    'Casual',
+    'Arcade',
+    'Card'
+  ];
+  let data = playstore;
+  const { sort, genres } = req.query;
 
-  res.send(playstore);
+  if (sort) {
+    if (!['rating', 'app'].includes(sort)) {
+      return res.status(400).send(`Sort must be rating or app`);
+    } else {
+      data = data.sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+    }
+  }
+
+  if (genres) {
+    if (genresList.includes(genres)) {
+      data = data.filter(element => {
+        return element.Genres.toLowerCase() === genres.toLowerCase();
+      });
+    } else {
+      res.status(404).send('You must supply a valid genre');
+    }
+  }
+  res.status(200).send(data);
 });
 app.listen(8080, () => {
   console.log('Server is running on port 8080');
